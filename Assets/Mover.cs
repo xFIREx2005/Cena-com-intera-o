e13252ee407abd6328273f16d3333d5106a2a994
xFIREx2005 <kaique.santos150@etec.sp.gov.br>
2021-05-18 08:07:25 -0300

    Arquivos do projeto da unity com interação

---
+++ Assets/Mover.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    bool isLeftButtonDown;
+    bool isRightButtonDown;
+    bool isMiddleButtonDown;
+    Vector3 m;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isLeftButtonDown = false;
+        isRightButtonDown = false;
+        isMiddleButtonDown = false;
+        m.x = 0.0f;
+        m.y = 0.0f;
+        m.z = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		
+
+			if (Input.GetKey(KeyCode.W))
+			{
+				m.x = 0.01f;
+				Movimento(m);
+				LogMessage("w");
+				m.x = 0.0f;
+			}
+			if (Input.GetKey(KeyCode.S))
+			{
+
+				m.x = -0.01f;
+				Movimento(m);
+				LogMessage("s");
+				m.x = 0.0f;
+
+			}
+			if (Input.GetKey(KeyCode.D))
+			{
+				m.z = -0.01f;
+				Movimento(m);
+				LogMessage("D");
+				m.z = 0.0f;
+			}
+			if (Input.GetKey(KeyCode.A))
+			{
+				m.z = 0.01f;
+				Movimento(m);
+				LogMessage("a");
+				m.z = 0.0f;
+		}
+			isLeftButtonDown = Input.GetMouseButtonDown(0);
+			isRightButtonDown = Input.GetMouseButtonDown(1);
+			isMiddleButtonDown = Input.GetMouseButtonDown(2);
+			if (isLeftButtonDown)
+			{
+				m.x = 0.5f;
+				m.y = 0.5f;
+				m.z = 0.5f;
+				Escala(m);
+				LogMessage("Botão esquerdo");
+				isLeftButtonDown = false;
+			}
+
+			if (isRightButtonDown)
+			{
+			m.x = -0.5f;
+			m.y = -0.5f;
+			m.z = -0.5f;
+			Escala(m);
+			LogMessage("Botão Direito");
+				isRightButtonDown = false;
+			}
+
+			if (isMiddleButtonDown)
+			{
+			LogMessage("Botão do meio");
+			isMiddleButtonDown = false;                                                                         
+			}	
+	}
+
+		void OnCollisionEnter(Collision Colider)
+		{
+			if (Colider.gameObject.tag == "Finish")
+			{
+				m.x = 0.1f;
+				m.y = 0.1f;
+				m.z = 0.1f;
+				Escala(m);
+			}
+		if (Colider.gameObject.tag == "Respawm")
+		{
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+
+
+			void Rotacao(Vector3 x)
+			{
+				transform.Rotate(x);
+			}
+
+			void Movimento(Vector3 m)
+			{
+				transform.Translate(m);
+
+			}
+			void Escala(Vector3 m)
+			{
+				transform.localScale += m;
+
+			}
+
+			void LogMessage(string msg)
+			{
+
+				Debug.Log("A opção selecionada foi " + msg.ToUpper());
+			}
+
+}
